3676fde305db35b803aa2a7a79312675e6374f6b
Brandon Theodore Ferrinov <19623223@std.stei.itb.ac.id>
2024-11-25 11:36:38 +0700

    merge

---
+++ src/Command/wordl3.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "boolean.h"
+#include "../ADT/mesinkata.h"
+#include "../ADT/mesinkarakter.h"
+#include "saldo.h"
+
+#ifndef WORDL3_H
+#define WORDL3_H
+
+#define MAX_ATTEMPTS_W 6
+#define NMax 50 
+#define W_COST 500
+
+boolean isWordEqual(Word word1, Word word2);
+boolean isValidWord(Word guess);
+void readGuess(Word *guess);
+void compareGuess(Word guess, Word secretWord, Word *result);
+void displayPreviousGuesses(Word results[], int attempts);
+void playWordle();
+
+#endif
---
+++ src/Command/work.h
@@ -0,0 +1,45 @@
+#ifndef WORK_H
+#define WORK_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "../ADT/list.h"
+#include "../ADT/mesinkata.h"
+#include "../ADT/mesinkarakter.h"
+#include "saldo.h"
+
+typedef struct {
+    Word name;
+    int income;
+    int duration;
+} Job;
+
+typedef struct {
+    Job jobs[5];
+    int count;
+} JobList;
+
+// Inisialisasi daftar pekerjaan
+void initializeJobs(JobList *jobList);
+
+// penyocokan 2 kata
+boolean isWordSimilar(Word a, Word b) ;
+
+// Menampilkan daftar pekerjaan
+void displayJobs(JobList jobList);
+
+// Mencari pekerjaan berdasarkan nama
+Job* findJob(JobList *jobList, Word jobName);
+
+// Melakukan pekerjaan
+void doWork(Job *selectedJob);
+
+// Membandingkan 2 kata
+boolean isWordEqual(Word a, Word b);
+
+// Mengkonversi Word ke string untuk ditampilakn
+void wordToString(Word word, char* str);
+
+#endif
+
